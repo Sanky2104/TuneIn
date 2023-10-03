b4c56cf5d118204c57f47b5cef1ba513e6d8003d
Sanky2104 <guptasanskriti21@gmail.com>
2023-10-03 23:18:09 +0530

    2nd changes of 3rd October 2023

--- src/components/Main.js
+++ src/components/Main.js
@@ -43,6 +43,24 @@ const Main = (props) => {
                             <img src="/images/ellipsis.svg" alt="" />
                         </button>
                     </SharedActor>
+                    <Description>Description</Description>
+                    <SharedImg>
+                        <a>
+                            <img src="/images/shared-image.jpg" alt="" />
+                        </a>
+                    </SharedImg>
+                    <SocialCounts>
+                        <li>
+                            <button>
+                                <img src="https://static-exp1.licdn.com/sc/h/d310t2g24pvdy4pt1jkedo4yb" alt="" />
+                                <img src="https://static-exp1.licdn.com/sc/h/5thsbmikm6a8uov24ygwd914f" alt="" />
+                                <span>75</span>
+                            </button>
+                        </li>
+                        <li>
+                            <a>2 comments</a>
+                        </li>
+                    </SocialCounts>
                 </Article>
             </div>
         </Container>
@@ -149,12 +167,73 @@ const SharedActor = styled.div`
         & > div {
             display: flex;
             flex-direction: column;
+            flex-grow: 1;
+            flex-basis: 0;
+            margin-left: 8px;
+            overflow: hidden;
+            span {
+                text-align: left;
+                &:first-child {
+                    font-size: 14px;
+                    font-weight: 700;
+                    color: rgba(0, 0, 0, 1);
+                }
+                &:nth-child(n + 1) {
+                    font-size:12px;
+                    color: rgba(0, 0, 0, 0.6);
+                }
+            }
         }
     }
     button {
+        position: absolute;
+        right: 12px;
+        top: 0;
+        background: transparent;
+        border: none;
+        outline: none;
         img {
             width: 2rem;
             height: 1.5rem;
+        }
+    }
+`;
+
+const Description= styled.div`
+    padding: 0 16px;
+    overflow: hidden;
+    color: rgba(0, 0, 0, 0.9);
+    font-size: 14px;
+    text-align: left;
+`;
+
+const SharedImg = styled.div`
+    margin-top: 8px;
+    width: 100%;
+    display: block;
+    position: relative;
+    background-color: #f9fafb;
+    img {
+        object-fit: contain;
+        width: 100%;
+        height: 100%;
+    }
+`;
+
+const SocialCounts = styled.ul`
+    line-height: 1.3;
+    display: flex;
+    align-items: flex-start;
+    overflow: auto;
+    margin: 0 16px;
+    padding: 8px 0;
+    border-bottom: 1 px solid #e9e5df;
+    list-style: none;
+    li {
+        margin-right: 5px;
+        font-size: 12px;
+        button {
+            display: flex;
         }
     }
 `;
